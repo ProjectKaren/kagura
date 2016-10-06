a38e477f580014f926dc4c8fc2f142756caf6b32
tamamu <tamamu.1r1s@gmail.com>
2016-10-06 22:51:22 +0900

    tmp

--- src/kagura.lisp
+++ src/kagura.lisp
@@ -8,5 +8,79 @@
   (:use :cl))
 (in-package :kagura)
 
-(cl-annot:enable-annot-syntax)
+;(cl-annot:enable-annot-syntax)
 ;; blah blah blah.
+
+(defstruct node
+  (name "" :type string)
+  (attributes (list) :type list)
+  (parent nil)
+  (children (make-array 1 :fill-pointer 0
+                          :adjustable t
+                          :element-type 'node)
+    :type (vector node)))
+
+(defstruct html-element
+  (tag "" :type string)
+  (attributes (list) :type list))
+
+(defstruct memory
+  (stack (make-array 8 :fill-pointer 0
+                       :adjustable t
+                       :element-type 'character)
+    :type (vector character))
+  (closep nil)
+  (tagp nil)
+  (current-node (make-node :token "Document"))
+  (state nil))
+
+(defmacro init-memory (mem)
+  `(setf (memory-stack ,mem) (make-array 8 :fill-pointer t :adjustable t :element-type 'character)
+         (memory-closep ,mem) nil
+         (memory-state ,mem) nil
+         (memory-tagp ,mem) nil))
+
+(setf *print-circle* t)
+
+(defun state-machine (data cursor mem)
+  (if (>= cursor (- (length data) 1))
+    (memory-current-node mem)
+    (let ((c (char data cursor))
+        (next-state nil))
+    (case c
+      (#\Space )
+      (#\< (if (null (memory-state mem))
+             (setf next-state :tag-open
+                   (memory-tagp mem) t)
+             (format t "irregular of tag-open")))
+      (#\/ (if (eq (memory-state mem) :tag-open)
+             (setf next-state :close-tag-open
+                   (memory-closep mem) t)
+             (format t "irregular of close-tag-open")))
+      (#\> (if (eq (memory-state mem) :tag-name)
+             (progn (setf next-state nil)
+                    (let ((current (memory-current-node mem)))
+                      (if (memory-closep mem)
+                        (setf (memory-current-node mem) (node-parent current))
+                        (let ((child (make-node :data
+                                       (make-html-element :tag (memory-stack mem))
+                                                :parent current)))
+                          (vector-push-extend child (node-children current))
+                          (setf (memory-current-node mem) child)))
+                      (init-memory mem)))
+             (format t "irregular of tag-close")))
+      (otherwise (if (or (eq (memory-state mem) :tag-open)
+                 (eq (memory-state mem) :close-tag-open)
+                 (eq (memory-state mem) :tag-name))
+           (setf next-state :tag-name))
+         (vector-push-extend c (memory-stack mem))))
+      (setf (memory-state mem) next-state)
+    (state-machine data (1+ cursor) mem)))
+  )
+
+(defvar *test*
+"<html><head><title>hage</title></head><body><h1>hoge</h1>hagehage</body></html>")
+
+
+(defun test ()
+  (state-machine *test* 0 (make-memory)))
